a4f2e316b1af8f4db31af75622070a877aca3afa
Roman <dstep.ds@gmail.com>
2025-07-19 15:29:24 +0300

    Рабочая версия. Не работает открытие фотографий.

--- src/main/java/com/example/tech_card_storage/controller/TechnologyCardController.java
+++ src/main/java/com/example/tech_card_storage/controller/TechnologyCardController.java
@@ -3,6 +3,10 @@ package com.example.tech_card_storage.controller;
 import com.example.tech_card_storage.model.TechnologyCard;
 import com.example.tech_card_storage.service.TechnologyCardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -27,6 +32,7 @@ public class TechnologyCardController {
     @GetMapping("/search")
     public String performSearch(@RequestParam("searchTerm") String searchTerm, Model model) {
         List<TechnologyCard> resultCards = techCardService.searchByCriteria(searchTerm);
+        resultCards.forEach(card -> card.setFilePath(extractFilenamePath(card.getFilePath())));
         model.addAttribute("resultCards", resultCards);
         return "search";
     }
@@ -39,7 +45,8 @@ public class TechnologyCardController {
             @RequestParam("fullName") String fullName
     ) throws IOException {
         if (!file.isEmpty()) {
-            String filePath = "uploads/" + file.getOriginalFilename();
+//            String filePath = "uploads/" + file.getOriginalFilename();
+            String filePath = "D:/upload-pics/" + file.getOriginalFilename();
             Files.write(Paths.get(filePath), file.getBytes());
 
             TechnologyCard card = new TechnologyCard(null, inventoryNumber, fullName, filePath);
@@ -48,5 +55,20 @@ public class TechnologyCardController {
             return "redirect:/search?searchTerm=" + fullName;
         }
         return "redirect:/upload";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) throws MalformedURLException {
+//        Resource resource = new UrlResource("file:" + System.getProperty("user.dir") + "/uploads" + filename);
+        Resource resource = new UrlResource("file:D:/upload-pics/" + filename);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+    private String extractFilenamePath(String filePath){
+        int lastSlashIndex = filePath.lastIndexOf('/');
+        return filePath.substring(lastSlashIndex + 1);
     }
 }
