3d71ec656202a5a32e84b9c0c9d653f1e8bd11eb
Roman Sergeev <roman.sergeev1993@gmail.com>
2025-07-21 15:54:58 +0300

    добавил пайплайн 3

--- src/test/java/com/example/tech_card_storage/TechCardStorageApplicationTests.java
+++ src/test/java/com/example/tech_card_storage/TechCardStorageApplicationTests.java
@@ -1,15 +1,20 @@
 package com.example.tech_card_storage;
 
+import com.example.tech_card_storage.repository.TechnologyCardRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class TechCardStorageApplicationTests {
+
+	@MockBean
+	private TechnologyCardRepository technologyCardRepository;
 
 	@Test
 	void contextLoads() {
 	}
-
 }
