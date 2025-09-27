ea2949d8e5ba993ccc4a99af6042326ef7abfdee
Roman <dstep.ds@gmail.com>
2025-09-28 00:19:45 +0300

    Изменил тип загружаемых файлов на pdf, с ним будет удобнее работать

---
+++ src/main/java/com/example/tech_card_storage/service/config/CardsFileTypeConfig.java
@@ -0,0 +1,10 @@
+package com.example.tech_card_storage.service.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class CardsFileTypeConfig {
+
+    public static final List<String> CARDS_FILE_TYPE_LIST = Arrays.asList("application/pdf");
+
+}
