76603ed8cdb140defefa7c47088bdfd7b20fc310
Roman <dstep.ds@gmail.com>
2025-10-06 13:45:18 +0300

    Добавил валидацию файла по размеру

--- src/main/java/com/example/tech_card_storage/service/config/CardsFileConfig.java
+++ src/main/java/com/example/tech_card_storage/service/config/CardsFileConfig.java
@@ -3,8 +3,10 @@ package com.example.tech_card_storage.service.config;
 import java.util.Arrays;
 import java.util.List;
 
-public final class CardsFileTypeConfig {
+public final class CardsFileConfig {
 
     public static final List<String> CARDS_FILE_TYPE_LIST = Arrays.asList("application/pdf");
+
+    public static final long CARDS_FILE_ALLOWED_SIZE = 10 * 1024 * 1024;
 
 }
